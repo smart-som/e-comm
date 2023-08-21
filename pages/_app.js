4c89eaed63c506f7a1cd7fc9c5a72e6eb4b0658f
Samson Ojukwu-Mba <fmba31@gmail.com>
2023-08-21 22:39:22 +0100

    ---Adding Shipping and payment pags

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { StoreProvider } from '@/Utils/Store';
+import { useRouter } from 'next/router';
 import '@/styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 
 export default function App({
   Component,
@@ -9,8 +10,29 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        <Component {...pageProps} />
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </StoreProvider>
     </SessionProvider>
   );
+}
+
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized?message=login required');
+    },
+  });
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  return children;
 }

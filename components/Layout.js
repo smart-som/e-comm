d1e4eae8baa5cc036419fa7d0675e773bdf8a1d3
Samson Ojukwu-Mba <fmba31@gmail.com>
2023-08-21 08:28:09 +0100

    update with login page

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,15 @@
+import { Store } from '@/Utils/Store';
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 export default function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
   return (
     <>
       <Head>
@@ -20,6 +27,11 @@ export default function Layout({ title, children }) {
             <div>
               <Link href="/cart" className="p-2">
                 Cart
+                {cartItemsCount > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cartItemsCount}
+                  </span>
+                )}
               </Link>
               <Link href="/login" className="p-2">
                 Login

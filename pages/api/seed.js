a3782e006da7e72a881751c941ffcc0dcf4da01b
Samson Ojukwu-Mba <fmba31@gmail.com>
2023-08-21 13:51:06 +0100

    adding database

---
+++ pages/api/seed.js
@@ -0,0 +1,12 @@
+import data from '@/Utils/data';
+import db from '@/Utils/db';
+import User from '@/models/User';
+
+const handler = async (req, res) => {
+  await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
+  await db.disconnect();
+  res.send({ message: 'seeded successfully' });
+};
+export default handler;

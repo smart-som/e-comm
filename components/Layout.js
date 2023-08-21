6fb9181e4753ba74632849cd23575b0f09565d8c
Samson Ojukwu-Mba <fmba31@gmail.com>
2023-08-21 21:32:59 +0100

    ---Adding dropdown menu and other updates

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,29 @@
 import { Store } from '@/Utils/Store';
 import Head from 'next/head';
 import Link from 'next/link';
+import Cookies from 'js-cookie';
 import React, { useContext, useEffect, useState } from 'react';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import { Menu } from '@headlessui/react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import DropdownLink from './DropdownLink';
 
 export default function Layout({ title, children }) {
   const { status, data: session } = useSession();
 
-  const { state } = useContext(Store);
+  const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
   useEffect(() => {
     setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
   }, [cart.cartItems]);
+
+  const logoutClickHandler = () => {
+    Cookies.remove('cart');
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
+  };
   return (
     <>
       <Head>
@@ -29,7 +38,7 @@ export default function Layout({ title, children }) {
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/" className="text-lg font-bold">
-              Lib-system
+              <p className="text-blue-600">Lib-system</p>
             </Link>
             <div>
               <Link href="/cart" className="p-2">
@@ -44,10 +53,38 @@ export default function Layout({ title, children }) {
               {status === 'loading' ? (
                 'Loading'
               ) : session?.user ? (
-                session.user.name
+                <Menu as="div" className="relative inline-block">
+                  <Menu.Button className="text-blue-600">
+                    {session.user.name}
+                  </Menu.Button>
+                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-slate-500 rounded-lg border border-gray-200 shadow-lg rounded">
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href="/profile">
+                        Profile
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <DropdownLink
+                        className="dropdown-link"
+                        href="/order-history"
+                      >
+                        Order History
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <h3
+                        className="dropdown-link"
+                        href="#"
+                        onClick={logoutClickHandler}
+                      >
+                        Logout
+                      </h3>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
               ) : (
-                <Link href="/login">
-                  <a className="p-2">Login</a>
+                <Link href="/login" className="p-2">
+                  Login
                 </Link>
               )}
             </div>
